0b625889db8f85f025caa048f52720a865fe7a8b
sepakdom <dominik.sepak1@resistant.ai>
2025-12-03 14:38:38 +0100

    Day 3 part 1 solution

--- 3/day3.jl
+++ 3/day3.jl
@@ -1,3 +1,6 @@
+include("../utils/read_input.jl")
+using .InputLoader
+
 function highestjoltage(bank::String)
     int_bank = [parse(Int, c) for c in bank]
     first_ix = argmax(int_bank[1:end-1])
@@ -8,4 +11,13 @@ end
 
 function totaljoltage(input)
     sum(highestjoltage(row) for row in eachline(IOBuffer(input)))
+end
+
+
+"""
+Solves day 3 part 1
+"""
+function main1()
+    input = read_input_file(input_file_path("day3.txt"))
+    println(totaljoltage(input))
 end
